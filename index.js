bd0391b8f83b3ba2aa67aff55095f0881659134f
Sailesh Kaveti <saileshkaveti@hotmail.com>
2021-03-20 00:21:19 -0700

    Initial coding done

---
+++ index.js
@@ -0,0 +1,44 @@
+const config = require('./config.json');
+const {
+    Client,
+    Attachment
+} = require('discord.js');
+var cron = require('node-cron');
+
+const bot = new Client();
+
+bot.login(config.token);
+
+bot.once('ready', () => {
+	console.log('Ready!');
+    const channel = bot.channels.cache.get('569320697873039379')
+    // Run every 10 seconds, when the time is past 7pm PST, send message to chat
+    //cron.schedule('* */5 * * * *', () => {
+        time = getTime();
+        //if (time.minutes > 15) {
+        channel.send(config.rat_end_message);
+        //}
+    //});
+});
+
+bot.on('message', message => {
+
+    let command = message.content.substring(config.prefix);
+
+	switch(command) {
+        case config.rat_end_message:
+            const fetchedChannel = message.guild.channels.cache.get('822725706738827284');
+            console.log(fetchedChannel);
+            fetchedChannel.delete();
+    }
+});
+
+function getTime(input) {
+    var date = input ? new Date(input) : new Date();
+    return {
+        hours : date.getHours(),
+        minutes : date.getMinutes(),
+        seconds : date.getSeconds(),
+        milliseconds : date.getMilliseconds()
+    }
+}
